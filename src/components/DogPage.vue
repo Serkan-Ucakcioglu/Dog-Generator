81a82fa45755e65af2d899e683ad16a536d38f59
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-07-21 09:06:11 +0300

    succes

--- src/components/DogPage.vue
+++ src/components/DogPage.vue
@@ -1,53 +1,57 @@
 <script setup lang="ts">
-import { ref } from "@vue/reactivity";
+import { reactive } from "@vue/reactivity";
 import { onMounted } from "@vue/runtime-core";
-import axios from "axios"
+import axios from "axios";
 
-const data = ref()
+
 
 interface apiType {
-  message: URL;
+  message: string;
 }
+
+
+const data: apiType = reactive({
+  message: '',
+})
+
 
 const getApi = () => {
-   axios.get<apiType>('https://dog.ceo/api/breeds/image/random')
-  .then( res => {
-    data.value = res.data
-  })
-}
+  axios.get<apiType>("https://dog.ceo/api/breeds/image/random").then((res) => {
+    data.message = res.data.message
+    console.log(data);
+  });
+};
 
-onMounted(getApi)
+onMounted(getApi);
 </script>
-
-
 
 <template>
   <div class="container">
     <div class="dog_wrapper">
       <button @click="getApi">Photo Change</button>
-    <img :src="data.message" alt="">
+      <img :src="data.message" alt="" />
     </div>
   </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.dog_wrapper{
+.dog_wrapper {
   display: flex;
   flex-direction: column;
   align-items: center;
-  button{
-    background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  cursor: pointer;
+  button {
+    background-color: #4caf50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
   }
-  img{
+  img {
     margin-top: 10px;
     width: 350px;
     height: 350px;
